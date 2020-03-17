0106b2fc866cfcbdfd87a34a73a16ecbe97d8a5a
kumarsai314 <62158775+kumarsai314@users.noreply.github.com>
2020-03-17 14:28:21 -0700

    Update dogsage.py

--- dogsage.py
+++ dogsage.py
@@ -1,4 +1,6 @@
 x= int(input('Enter a human age:'))
-y= 13+4*x
-
-print('Dogs age in dogs year',y)
+if x==1:
+    print('Dogs age in year 10.5')
+else:
+    y= 13+4*x
+    print('Dogs age in dogs year',y)
